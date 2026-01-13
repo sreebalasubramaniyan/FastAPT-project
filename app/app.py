c8f14dd9df5311f6eab86f4e332b53652da42a10
sreebalasubramaniyan <sreebalasubramaniyan682@gmail.com>
2026-01-13 22:30:57 +0530

    created an app and learnt about @app.get() and and end points

--- app/app.py
+++ app/app.py
@@ -1,3 +1,28 @@
 from fastapi import FastAPI 
 # creating an object
 app = FastAPI()
+# get(endpoint/path)
+
+
+#1)creating simple end point
+# @app.get("/hello-world") # @ is decorator
+# def hello_world():
+#     return {"message":"Hello World"} # data/JSON - pydantic or python dictionary
+
+#2)creating text_post endpoint
+
+text_posts = {
+    1 : {
+        "title" : "New Post",
+        "content" : "cool test post"
+       }
+}
+# get all posts
+@app.get("/posts")
+def get_all_posts():
+    return text_posts
+# get individual post with their id
+@app.get("/posts/{id}")
+# in function also id has to be given
+def get_post_by_id(id:int):
+    return text_posts[id]
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from fastapi-tutorial!")
-
+# to run an api we need to import uvicorn
+import uvicorn
 
 if __name__ == "__main__":
-    main()
+    # app
+    # host - domain we run our api(0.0.0 means available local host, we can access it and one who knows the private key also can acces this )
+    # port - 8000 access /hello-world
+    # reload - everytime we  update the file the server will restart
+    uvicorn.run(app="app.app:app",host="127.0.0.1",port=8000,reload=True)
+
+    # 127.0.0.1/docs - /docs its an endpoint
+        # go and execute and see, the response from our API app that we created
+    # 127.0.0.1/redoc - similar version of above endpoint
+    # or we can also use http://localhost:8000/hello-world
+
